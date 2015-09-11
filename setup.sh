093e1b95f484fe887b92ab989d29d8a62e38c713
rotlogix <rotlogix@gmail.com>
2015-09-11 16:12:27 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,9 +4,11 @@
 pip2.7 install --upgrade pip
 
 # Install requirements
+echo "[*] Installing requirements"
 pip2.7 install -r requirements
 
 # Download Androguard
+echo "[*] Cloning androguard"
 git clone https://github.com/androguard/androguard.git
 
 # Setup Androguard
@@ -16,6 +18,7 @@ python setup.py install
 cd ..
 
 # Download and build apktool
+echo "[*] Downloading and building the apktool "
 git clone git://github.com/iBotPeaches/Apktool.git
 cd Apktool
 ./gradlew build fatJar
@@ -24,11 +27,13 @@ cd ..
 rm -rf Apktool/
 
 # Download and unzip dex2jar
+echo "[*] Downloading dex2jar"
 wget https://bitbucket.org/pxb1988/dex2jar/downloads/dex2jar-2.0.zip
 unzip dex2jar-2.0.zip
 rm dex2jar-2.0.zip
 chmod +x dex2jar-2.0/*
 
 # Download frida-server
+echo "[*] Downloading Frida"
 curl -O http://build.frida.re/frida/android/arm/bin/frida-server
 chmod +x frida-server
