1354cd8505d81a115d671253d8588a39f968959d
rotlogix <rotlogix@gmail.com>
2015-09-18 09:48:26 -0400

    Fixed major feature issues in the storage module for the surgicalAPI

--- framework/brains/surgical/storage.py
+++ framework/brains/surgical/storage.py
@@ -7,13 +7,23 @@ t = Terminal()
 
 class InsecureStorageEnum(object):
 
-    values = [
+    values = {
 
-        "getExternalFilesDir",
-        "getSharedPreferences",
-        "openFileOutput"
-        "openFileInput"
-    ]
+        "android.os.Environment": [
+
+            "getDataDirectory",
+            "getDownloadCacheDirectory",
+            "getExternalStorageDirectory",
+            "getExternalStoragePublicDirectory",
+            "getExternalStorageState",
+            "getRootDirectory",
+            "getStorageState",
+            "isExternalStorageEmulated",
+            "isExternalStorageRemovable"
+
+        ]
+
+    }
 
 
 class InsecureStorage(object):
@@ -39,31 +49,28 @@ class InsecureStorage(object):
             print(t.green("[{0}] ".format(datetime.now()) + t.yellow("Performing surgery ...")))
             # Get enum values
             #
-            for v in self.enum.values:
-                # This returns PathP
-                # objects into a list
-                #
-                paths = x.get_tainted_packages().search_methods("android.content.Context", "{0}".format(v), ".")
+            for a, b in self.enum.values.items():
+                for c in b:
+                    paths = x.get_tainted_packages().search_methods("{0}".format(b), "{0}".format(c), ".")
+                    if paths:
+                        for p in paths:
+                            for method in self.apks.get_methods():
+                                if method.get_name() == p.get_src(vm.get_class_manager())[1]:
+                                    if method.get_class_name() == p.get_src(vm.get_class_manager())[0]:
 
-                if paths:
-                    for p in paths:
-                        for method in self.apks.get_methods():
-                            if method.get_name() == p.get_src(vm.get_class_manager())[1]:
-                                if method.get_class_name() == p.get_src(vm.get_class_manager())[0]:
+                                        mx = x.get_method(method)
+                                        d = decompile.DvMethod(mx)
+                                        d.process()
 
-                                    mx = x.get_method(method)
-                                    d = decompile.DvMethod(mx)
-                                    d.process()
+                                        print(t.green("[{0}] ".format(datetime.now()) +
+                                              t.yellow("Found: ") +
+                                              "{0}".format(c)))
+                                        print(t.green("[{0}] ".format(datetime.now()) +
+                                                      t.yellow("Class: ") +
+                                                      "{0}".format(method.get_class_name())))
+                                        print(t.green("[{0}] ".format(datetime.now()) +
+                                                      t.yellow("Method: ") +
+                                                      "{0}".format(method.get_name())))
 
-                                    print(t.green("[{0}] ".format(datetime.now()) +
-                                          t.yellow("Found: ") +
-                                          "{0}".format(v)))
-                                    print(t.green("[{0}] ".format(datetime.now()) +
-                                                  t.yellow("Class: ") +
-                                                  "{0}".format(method.get_class_name())))
-                                    print(t.green("[{0}] ".format(datetime.now()) +
-                                                  t.yellow("Method: ") +
-                                                  "{0}".format(method.get_name())))
-
-                                    print(method.show())
-                                    print(d.get_source())
+                                        print(method.show())
+                                        print(d.get_source())
